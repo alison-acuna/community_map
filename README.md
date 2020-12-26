4848e7aa1486e06a3cede8344b8ab9331ac272e9
aacuna <aacuna@newrelic.com>
2020-12-26 14:37:16 -0800

    Moved to psql

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # community_map
+
+https://medium.com/@thorntonbrenden/ruby-on-rails-switch-from-sqlite3-to-postgres-590009645c25#:~:text=Update%20Gemfile,and%20install%20the%20postgres%20gem.&text=Once%20you've%20updated%20your,lock%20file.
--- community_map/db/schema.rb
+++ community_map/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20201220223919) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "people", force: :cascade do |t|
     t.string   "name"
     t.string   "pronouns"
