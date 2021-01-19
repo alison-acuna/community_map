6a6cc4d4296583c8a651f9cd06112480b1747e89
aacuna <aacuna@newrelic.com>
2021-01-19 08:25:32 -0800

    WIP Add home page for easier navigation

--- community_map/config/routes.rb
+++ community_map/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "people#index"
   resources :people
   resources :tags
+  get "/pages/:page" => "pages#show"
   # get "/people", to: "people#index"
   # get "/people/:id", to: "people#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
